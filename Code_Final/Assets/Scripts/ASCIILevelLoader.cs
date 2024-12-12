940dbafdc49f28a422c3a02f8eb12b42e1a4db0e
bunibell <158766692+bunibell@users.noreply.github.com>
2024-12-12 15:21:49 -0500

    created sprites

--- Code_Final/Assets/Scripts/ASCIILevelLoader.cs
+++ Code_Final/Assets/Scripts/ASCIILevelLoader.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    //Variables 
+    public float xoffset;
+    public float yoffset;
+
+    public GameObject player;
+    public GameObject wall;
+    public GameObject obstacle;
+    public GameObject goal;
+
+    public GameObject currentPlayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
