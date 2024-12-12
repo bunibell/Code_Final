652774716c79b93c2511b4ab373361d31144da93
bunibell <158766692+bunibell@users.noreply.github.com>
2024-12-12 16:15:58 -0500

    created ASCII map

--- Code_Final/Assets/Scripts/ASCIILevelLoader.cs
+++ Code_Final/Assets/Scripts/ASCIILevelLoader.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -15,16 +17,90 @@ public class NewBehaviourScript : MonoBehaviour
 
     public GameObject currentPlayer;
 
+    Vector2 startPos;
+
+    public string fileName;
+
+    public int currentLevel = 0;
+
+    public GameObject level;
+
+    public int CurrentLevel
+    { 
+        get { return currentLevel; }
+        set
+        {
+            currentLevel = value;
+            LoadLevel();
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadLevel()
     {
-        
+        Destroy(level);
+
+        level = new GameObject("Level");
+
+        string current_file_path = Application.dataPath + "/Resources/" + fileName.Replace("Num", currentLevel + "");
+
+        string[] fileLines = File.ReadAllLines(current_file_path);
+
+        for (int y = 0; y < fileLines.Length; y++)
+        {
+            string lineText = fileLines[y];
+
+            char[] characters = lineText.ToCharArray();
+
+            for (int x = 0; x < characters.Length; x++)
+            {
+                char c = characters[x];
+
+                GameObject newObject;
+
+                switch (c)
+                {
+                    case 'p':
+                        newObject = Instantiate<GameObject>(player);
+                        if (currentPlayer = null)
+                            currentPlayer = newObject;
+
+                        startPos = new Vector2(x + xoffset, y - y + yoffset);
+                        break;
+
+                    case 'w':
+                        newObject = Instantiate<GameObject>(wall);
+                        break;
+
+                    case '*':
+                        newObject = Instantiate<GameObject>(obstacle);
+                        break;
+
+                    case '&':
+                        newObject = Instantiate<GameObject>(goal);
+                        break;
+
+                    default:
+                        newObject = null; 
+                        break;
+
+                }
+
+                if (newObject != null)
+                {
+                    if (!newObject.name.Contains("Player"))
+                    {
+                        newObject.transform.parent = level.transform;
+                    }
+
+                    newObject.transform.position = new Vector3(x + xoffset, -y + yoffset, 0);
+                }
+            }
+        }
     }
 }
--- Code_Final/Assets/Scripts/GameManager.cs
+++ Code_Final/Assets/Scripts/GameManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     //Variables 
     public static GameManager instance;
+
+    //private bool canstart = true;
 
 
     private void Awake()
@@ -21,4 +24,13 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    /*public void StartGame()
+    {
+        if (canstart)
+        {
+            SceneManager.LoadScene(1);
+            canstart = false;
+        }
+    }*/
 }
