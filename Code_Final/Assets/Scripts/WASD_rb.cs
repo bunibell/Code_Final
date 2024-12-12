466c354bbf73fa254612bdce1be2fd2735948b3c
bunibell <158766692+bunibell@users.noreply.github.com>
2024-12-12 16:37:31 -0500

    fixed char controller

--- Code_Final/Assets/Scripts/WASD_rb.cs
+++ Code_Final/Assets/Scripts/WASD_rb.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class WASD_rb : MonoBehaviour
 {
+
+    //variables 
+    public float forceAmt = 2f;
+
+    private int i = 8;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D)) 
+        { 
+            rb.AddForce(Vector2.right * forceAmt);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector2.down * forceAmt);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * forceAmt);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * forceAmt);
+        }
+
     }
 }
